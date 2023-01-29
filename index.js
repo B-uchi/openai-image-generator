9aa3b0402b5434d1dc99ceb489860da392142e49
Umeano Buchi <108285540+B-uchi@users.noreply.github.com>
2023-01-29 09:58:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config('./etc/secrets/.env');
 const port = process.env.PORT || 5000;
 
 const app = express();
